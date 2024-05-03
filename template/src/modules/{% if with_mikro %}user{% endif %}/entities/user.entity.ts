64364d8c5942a7508245b2ef90e2f3948954efe5
Garrett HE <garrett.he@outlook.com>
2024-05-03 11:26:44 +0800

    feat: add support for mikro-orm

---
+++ template/src/modules/{% if with_mikro %}user{% endif %}/entities/user.entity.ts
@@ -0,0 +1,10 @@
+import {Entity, PrimaryKey, Property} from "@mikro-orm/core";
+
+@Entity({tableName: "users"})
+export class User {
+    @PrimaryKey()
+    public id!: string;
+
+    @Property()
+    public name!: string;
+}
