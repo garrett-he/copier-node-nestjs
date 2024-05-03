2c49e20d65932b1e42f3fb08599ffb185505ec65
Garrett HE <garrett.he@outlook.com>
2024-05-03 10:27:51 +0800

    feat: create simpliest service with nestjs

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,3 +1,16 @@
-import {sayHello} from "./example.ts";
+import {NestFactory} from "@nestjs/core";
+import {ApplicationModule} from "./modules/app.module";
 
-console.log(sayHello("You"));
+const DEFAULT_PORT = 3000;
+
+async function bootstrap(): Promise<void>
+{
+    const app = await NestFactory.create(ApplicationModule);
+
+    await app.listen(process.env["PORT"] || DEFAULT_PORT);
+}
+
+bootstrap().catch(err => {
+    console.error(err);
+    process.exit(1);
+});
